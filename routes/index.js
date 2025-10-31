4ca4e14f40d1246044d5bd4968bb6adab01e89fd
Kierownik223 <marcel@marmak.net.pl>
2025-10-31 19:47:20 +0100

    Add proper Cache-Control header on home page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import redirectController from "../controllers/redirect.js";
 const Router = express.Router();
 
 Router.get("/", (req, res) => {
+    res.set('Cache-Control', 'no-cache');
     res.render("home");
 });
 
